57747c4a9b8562a721960c36adc106172a62788d
hanyazou <hanyazou@gmail.com>
2024-05-18 12:44:52 +0900

    fw: sdcard_test: Add FATFS sample code
    Tutorial: An SD card over SPI using STM32CubeIDE and FatFS
    https://01001000.xyz/2020-08-09-Tutorial-STM32CubeIDE-SD-card/

--- FW/sdcard_test/Core/Src/main.c
+++ FW/sdcard_test/Core/Src/main.c
@@ -112,58 +112,86 @@ int main(void)
   /* USER CODE BEGIN 2 */
   HAL_TIM_Base_Start(&htim1);
 
-  uint8_t i2c_found_ds1307 = 0;
-  printf("I2C scanner\r\n");
-  HAL_StatusTypeDef result;
-  for (uint8_t addr = 0; addr < 128; addr++) {
-    if (addr % 16 == 0) {
-        printf("%02X:", addr);
-    }
-    if (addr == 0) {
-        printf("   ");
-        continue;
-    }
-    result = HAL_I2C_IsDeviceReady(&hi2c1, (uint16_t)(addr << 1), 2, 2);
-    if (result == HAL_OK) {
-        printf(" %02X", addr);
-        if ((addr << 1) == DS1307_ADDR) {
-            i2c_found_ds1307 = 1;
-        }
-    } else {
-        printf(" __");
-    }
-    if (addr % 16 == 15) {
-        printf("\r\n");
-    }
-  }
-  printf("\r\n");
+myprintf("\r\n~ SD card demo by kiwih ~\r\n\r\n");
 
-  if (i2c_found_ds1307) {
-    uint8_t datetime[7];
-    static uint8_t prev_datetime[7] = { 0 };
-    int i = 0;
+  HAL_Delay(1000); //a short delay is important to let the SD card settle
 
-    printf("DS1307 RTC test\r\n");
-    datetime[0] = 0x45;
-    datetime[1] = 0x30;
-    datetime[2] = 0x13;
-    datetime[3] = 0x12;
-    datetime[4] = 0x05;
-    datetime[5] = 0x06;
-    datetime[6] = 0x24;
-    result = HAL_I2C_Mem_Write(&hi2c1, DS1307_ADDR, 0x00, I2C_MEMADD_SIZE_8BIT,
-                               datetime, sizeof(datetime), 1000);
-    while (i < 14) {
-      result = HAL_I2C_Mem_Read(&hi2c1, DS1307_ADDR, 0x00, I2C_MEMADD_SIZE_8BIT,
-                                datetime, sizeof(datetime), 1000);
-      if (memcmp(datetime, prev_datetime, sizeof(datetime)) != 0) {
-        memcpy(prev_datetime, datetime, sizeof(datetime));
-        printf("20%02X/%02X/%02X %02X:%02X:%02X \r\n",
-               datetime[6], datetime[5], datetime[4], datetime[2], datetime[1], datetime[0]);
-        i++;
-      }
-    }
+  //some variables for FatFs
+  FATFS FatFs; 	//Fatfs handle
+  FIL fil; 		//File handle
+  FRESULT fres; //Result after operations
+
+  //Open the file system
+  fres = f_mount(&FatFs, "", 1); //1=mount now
+  if (fres != FR_OK) {
+	myprintf("f_mount error (%i)\r\n", fres);
+	while(1);
   }
+
+  //Let's get some statistics from the SD card
+  DWORD free_clusters, free_sectors, total_sectors;
+
+  FATFS* getFreeFs;
+
+  fres = f_getfree("", &free_clusters, &getFreeFs);
+  if (fres != FR_OK) {
+	myprintf("f_getfree error (%i)\r\n", fres);
+	while(1);
+  }
+
+  //Formula comes from ChaN's documentation
+  total_sectors = (getFreeFs->n_fatent - 2) * getFreeFs->csize;
+  free_sectors = free_clusters * getFreeFs->csize;
+
+  myprintf("SD card stats:\r\n%10lu KiB total drive space.\r\n%10lu KiB available.\r\n", total_sectors / 2, free_sectors / 2);
+
+  //Now let's try to open file "test.txt"
+  fres = f_open(&fil, "test.txt", FA_READ);
+  if (fres != FR_OK) {
+	myprintf("f_open error (%i)\r\n");
+	while(1);
+  }
+  myprintf("I was able to open 'test.txt' for reading!\r\n");
+
+  //Read 30 bytes from "test.txt" on the SD card
+  BYTE readBuf[30];
+
+  //We can either use f_read OR f_gets to get data out of files
+  //f_gets is a wrapper on f_read that does some string formatting for us
+  TCHAR* rres = f_gets((TCHAR*)readBuf, 30, &fil);
+  if(rres != 0) {
+	myprintf("Read string from 'test.txt' contents: %s\r\n", readBuf);
+  } else {
+	myprintf("f_gets error (%i)\r\n", fres);
+  }
+
+  //Be a tidy kiwi - don't forget to close your file!
+  f_close(&fil);
+
+  //Now let's try and write a file "write.txt"
+  fres = f_open(&fil, "write.txt", FA_WRITE | FA_OPEN_ALWAYS | FA_CREATE_ALWAYS);
+  if(fres == FR_OK) {
+	myprintf("I was able to open 'write.txt' for writing\r\n");
+  } else {
+	myprintf("f_open error (%i)\r\n", fres);
+  }
+
+  //Copy in a string
+  strncpy((char*)readBuf, "a new file is made!", 19);
+  UINT bytesWrote;
+  fres = f_write(&fil, readBuf, 19, &bytesWrote);
+  if(fres == FR_OK) {
+	myprintf("Wrote %i bytes to 'write.txt'!\r\n", bytesWrote);
+  } else {
+	myprintf("f_write error (%i)\r\n");
+  }
+
+  //Be a tidy kiwi - don't forget to close your file!
+  f_close(&fil);
+
+  //We're done, so de-mount the drive
+  f_mount(NULL, "", 0);
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
